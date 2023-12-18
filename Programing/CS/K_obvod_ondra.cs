b4489d05a008c8619968b68cbe8925be880ea9a4
DarkScarCZ <82397357+DarkScarCZ@users.noreply.github.com>
2023-12-18 13:31:25 +0100

    Update K_obvod_ondra.cs

--- Programing/CS/K_obvod_ondra.cs
+++ Programing/CS/K_obvod_ondra.cs
@@ -1,5 +1,6 @@
-﻿float pi = 3.14f;
-for (float polomer = 0f; polomer < 50; polomer = polomer+ 1 * 2 * pi)
+int max = Convert.ToInt32(Console.ReadLine());
+float pi = 3.14f;
+for (float polomer = 0f; polomer < max; polomer = polomer+ 1 * 2 * pi)
 {
     Console.WriteLine(polomer);
     
