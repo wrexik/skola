396fde02caee78336a63a0da4d57e9d944cfbdb7
Wrexik <74264557+wrexik@users.noreply.github.com>
2024-01-15 12:37:09 +0100

    Update pole_edit.cs

--- Programing/CS/pole_edit.cs
+++ Programing/CS/pole_edit.cs
@@ -1,6 +1,25 @@
-int[] words = new int[10];
-words[2] = 69;
+using System.Security.Authentication.ExtendedProtection;
 
-int ven = words[2];
+string[] words = new string[10];
+Console.WriteLine("Chcete pokracovat?");
+string pokracovat = Console.ReadLine();
 
-Console.WriteLine(ven);
+
+while (pokracovat == "ano") {
+    Console.WriteLine("ON");
+
+    Console.WriteLine("Kde chcete změnit písmeno?");
+    int num_list = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Co to bude za písmeno?");
+    string num_change = Console.ReadLine();
+
+    words[num_list] = num_change;
+
+    Console.Write("Uspesne zmeneno: {0} na {1}", num_list, num_change);
+
+    Console.WriteLine(words[num_list]);
+
+    ///Console.WriteLine("Chcete pokracovat?");
+    ///string pokracovat = Console.ReadLine();
+}
