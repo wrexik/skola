3e0fea0d338a755c752f2539ff154a4d90f7eea8
Wrexik <74264557+wrexik@users.noreply.github.com>
2024-01-22 12:00:21 +0100

    FORMAT YOU BITCH

--- Programing/CS/pole_edit.cs
+++ Programing/CS/pole_edit.cs
@@ -4,9 +4,10 @@ try
     Console.WriteLine("Zadejte počet políček v poli");
     int p = Convert.ToInt32(Console.ReadLine());
     p = p + 1;
-    string[] words = new string[p];
+    string[] words = new string[p]; // zacne list s velikosti zadane cislo
 
-    while (true)
+
+    while (true)    // loop
     {
         try
         {
@@ -16,37 +17,31 @@ try
             Console.WriteLine("Co to bude za písmeno?");
             string num_change = Console.ReadLine();
 
-            words[Convert.ToUInt32(num_list)] = num_change;
+            words[Convert.ToUInt32(num_list)] = num_change; // prida moznosti na dane pole do listu
 
             Console.WriteLine("Uspesne zmeneno: {0} na {1}", num_list, words[Convert.ToUInt32(num_list)]);
 
             Console.WriteLine("Chcete pokracovat?");
-            string pokracovat = Console.ReadLine();
+            string pokracovat = Console.ReadLine(); // prompt na opakovani / nove hodnoty
 
             if (pokracovat == "ne")
             {
-
-
                 Console.Write(" ");
                 int i = -1;
 
-                foreach (string word in words)
+                foreach (string word in words) // vypis vsech radku v poli + cislo
                 {
                     i = i + 1;
                     Console.Write(i);
                     Console.Write(" ");
                     Console.WriteLine(word);
                 }
-                break;
+                break;  // ne == konec
             }
         }
-
-
         catch
         {
-
             Console.WriteLine("Špatný vstup");
-
         }
 
     }
@@ -58,3 +53,5 @@ catch (Exception)
     Console.WriteLine("");
     goto All;
 }
+
+//done EZ
