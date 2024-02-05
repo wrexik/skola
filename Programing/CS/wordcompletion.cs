1d04bf949900f1cf9009806f4128acfe8ef420ed
Lukáš S <69255581+MortikCZ@users.noreply.github.com>
2024-02-05 15:31:10 +0100

    Update wordcompletion.cs

--- Programing/CS/wordcompletion.cs
+++ Programing/CS/wordcompletion.cs
@@ -1,34 +1,39 @@
-List<string> wordlist = new List<string> { "mokry", "maso", "mlynar", "maso", "makarom", "masozravy" };
-string[] wordsarray = wordlist.ToArray();
+List<string> wordlist = new List<string> { "mokry", "maso", "mlynar", "maso", "makarom", "masozravy" }; //Seznam slov našeptávače ve stringu
+string[] wordsarray = wordlist.ToArray(); // Převod slov z seznamu slov ve stringu do pole
 
-Console.WriteLine("Zadejte klicova pismena pro zahajeni vyhledavani naseptavace mezi slovy");
+// Zadání klíčových slov do našeptávače
+Console.WriteLine("Zadejte klicova pismena pro zahajeni vyhledavani naseptavace mezi slovy"); 
 string input = Console.ReadLine();
 
+// While loop pro opakované hledání slov
 while (true)
 {
-    foreach (string word in wordsarray)
+    if (!string.IsNullOrEmpty(input))
     {
-        if (word.StartsWith(input))
+        foreach (string word in wordsarray)  // Vypsání nalezených slov
         {
-            Console.WriteLine("Nalezené slovo: " + word);
+            if (word.StartsWith(input))
+            {
+                Console.WriteLine("Nalezené slovo: " + word);  
+            }
         }
-}
+    }
 
-    Console.WriteLine("Chcete pokracovat ve vyhledavani (ano/ne)?");
+    Console.WriteLine("Chcete pokracovat ve vyhledavani (ano/ne)?"); // Otázka pro pokračování ve vyhledávání slov
     string response = Console.ReadLine();
 
-    if (response == "ne")
+    if (response.ToLower() == "ne")  // Odpověď ne ukončí loop
     {
         Console.WriteLine("Vyhledavani naseptavace bylo ukonceno");
         break;
     }
-    else if (response == "ano")
+    else if (response.ToLower() == "ano") // Odpověď ano -> program se zeptá na jiná klíčová slova
     {
-        Console.WriteLine("Zadejte klicova pismena pro zahajeni vyhledavani naseptavace mezi slovy");
+        Console.WriteLine("Zadejte klicova pismena pro zahajeni vyhledavani naseptavace mezi slovy"); //Zadání klíčových slov do našeptávače
         input = Console.ReadLine();
     }
     else
     {
-        Console.WriteLine("Neplatná odpověď. Zadejte 'ano' nebo 'ne'.");
+        Console.WriteLine("Neplatná odpověď. Zadejte 'ano' nebo 'ne'.");  // Zde je chyba, kdy dojde k vypsání původně nalezených slov hned za příkazem "Console.WriteLine", bohužel jsem to nedokázal vyřešit.
     }
 }
