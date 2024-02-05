fbdcd7dc4f35ebf55fba584e15484440c308901e
Lukáš Starec <Starec.Lukas@sosasou.cz>
2024-02-05 12:25:41 +0100

    add

---
+++ Programing/CS/wordcompletion.cs
@@ -0,0 +1,13 @@
+List<string> wordlist = new List<string> { "mokry", "maso", "mlynar", "maso", "makarom", "masozravy" };
+string[] wordsarray = wordlist.ToArray();
+
+Console.WriteLine("Zadejte neco");
+string input = Console.ReadLine();
+
+foreach (string word in wordsarray)
+{
+	if (word.StartsWith(input))
+	{
+		Console.WriteLine("Nalezené slovo: " + word);
+	}
+}
