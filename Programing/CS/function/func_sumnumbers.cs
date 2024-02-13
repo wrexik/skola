d1d1449e0639d8dc08195a6366470f2f6c62f689
MortikCZ <Starec.Lukas@sosasou.cz>
2024-02-13 11:20:03 +0100

    add func_sumnumbers.cs

---
+++ Programing/CS/function/func_sumnumbers.cs
@@ -0,0 +1,20 @@
+﻿// Zadání prvního čísla
+Console.WriteLine("Zadej prvni cislo pro soucet:");
+string input1 = Console.ReadLine();
+int num1 = int.Parse(input1);
+
+// Zadání druhého čísla
+Console.WriteLine("Zadej druhe cislo pro soucet:");
+string input2 = Console.ReadLine();
+int num2 = int.Parse(input2);
+
+// Vypsání součtu dvou čísel
+int sum = Soucet(num1, num2);
+Console.WriteLine("Soucet dvou zadanych cisel -> {0}", sum);
+
+// Funkce pro součet dvou zadaných čísel
+static int Soucet(int num1, int num2)
+{
+	int sum = num1 + num2;
+	return sum;
+}
