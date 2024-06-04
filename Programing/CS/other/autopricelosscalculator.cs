8d27147d07b6f35c5fa6d17986de6933bc9eba15
Lukáš Starec <Starec.Lukas@sosasou.cz>
2024-06-04 11:16:23 +0200

    další autíčkovskej update

--- Programing/CS/other/autopricelosscalculator.cs
+++ Programing/CS/other/autopricelosscalculator.cs
@@ -1,74 +1,112 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Threading;
 
 class Program
 {
     static void Main()
     {
-        // Výpis uvítací zprávy a oddělovací čáry
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
         Console.WriteLine("Vítejte v programu pro výpočet roční ztráty ceny vozu.");
+        Console.WriteLine();
+        Console.WriteLine("Prosím dodržujte pokyny programu, v opačném případě dojde k chybě!");
+        Console.WriteLine();
         Console.WriteLine(new string('-', 50));
-
-        // Vytvoření seznamu pro uchování informací o vozech
-        List<Car> cars = new List<Car>();
-
-        // Cyklus pro zadávání informací o vozech
-        while (true)
-        {
-            // Zadání názvu vozu nebo ukončení zadávání
-            Console.Write("Zadejte název vozu (nebo 'konec' pro ukončení zadávání): ");
-            string name = Console.ReadLine();
-            if (name.ToLower() == "konec") break;
-
-            // Zadání původní ceny vozu
-            Console.Write("Zadejte původní cenu vozu: ");
-            decimal originalPrice = decimal.Parse(Console.ReadLine());
-
-            // Zadání procentuální roční ztráty ceny vozu
-            Console.Write("Zadejte procentuální roční ztrátu ceny vozu: ");
-            decimal annualLossPercentage = decimal.Parse(Console.ReadLine());
-
-            // Přidání vozu do seznamu
-            cars.Add(new Car { Name = name, PurchaseYear = 2025, OriginalPrice = originalPrice, AnnualLossPercentage = annualLossPercentage });
-        }
-
-        // Výpis hlavičky pro výsledky
-        Console.WriteLine("\nProcentuální ztráta ceny vozu od roku 2025 do roku 2030:");
-        Console.WriteLine(new string('-', 50));
-
-        // Výpis názvů vozů
-        Console.Write("Rok".PadRight(15));
-        foreach (var car in cars)
-        {
-            Console.Write($"{car.Name.PadRight(15)}");
-        }
         Console.WriteLine();
 
-        // Výpis cen pro každý rok
-        for (int year = 2025; year <= 2030; year++)
+        while (true)
         {
-            Console.Write($"{year}".PadRight(15));
-            foreach (var car in cars)
+            Console.WriteLine("Pokud si přejete pokračovat, stiskněte ENTER!");
+
+            if (Console.ReadKey(true).Key == ConsoleKey.Enter)
             {
-                // Výpočet ceny vozu pro daný rok
-                decimal currentPrice = car.OriginalPrice;
-                for (int y = car.PurchaseYear; y < year; y++)
+                Console.Clear();
+
+                Console.Write("Zadejte počet aut: ");
+                int carCount = int.Parse(Console.ReadLine());
+
+                string[] carNames = new string[carCount];
+                decimal[] originalPrices = new decimal[carCount];
+                decimal[] annualLossPercentages = new decimal[carCount];
+                decimal[] annualServiceCosts = new decimal[carCount];
+                decimal[] annualFuelCosts = new decimal[carCount];
+                decimal[] totalLosses = new decimal[carCount];
+
+                for (int i = 0; i < carCount; i++)
                 {
-                    currentPrice -= currentPrice * (car.AnnualLossPercentage / 100);
+                    Console.WriteLine($"\nAuto č. -> {i + 1}:");
+
+                    Console.Write("Zadejte název vozu: ");
+                    carNames[i] = Console.ReadLine();
+
+                    Console.Write("Zadejte původní cenu vozu: ");
+                    originalPrices[i] = decimal.Parse(Console.ReadLine());
+
+                    Console.Write("Zadejte procentuální roční ztrátu ceny vozu: ");
+                    annualLossPercentages[i] = decimal.Parse(Console.ReadLine());
+
+                    Console.Write("Zadejte roční cenu za servis: ");
+                    annualServiceCosts[i] = decimal.Parse(Console.ReadLine());
+
+                    Console.Write("Zadejte roční cenu za benzín: ");
+                    annualFuelCosts[i] = decimal.Parse(Console.ReadLine());
                 }
-                // Výpis ceny vozu
-                Console.Write($"{currentPrice:0.00}".PadRight(15));
+
+                Console.WriteLine();
+
+                Console.WriteLine("\nRoční ztráta ceny vozu od roku 2025 do roku 2030:");
+                Console.WriteLine(new string('-', 50));
+
+                Console.Write("Rok".PadRight(15));
+                foreach (var carName in carNames)
+                {
+                    Console.Write($"{carName.PadRight(15)}");
+                }
+                Console.WriteLine();
+
+                for (int year = 2025; year <= 2030; year++)
+                {
+                    Console.Write($"{year}".PadRight(15));
+                    for (int i = 0; i < carCount; i++)
+                    {
+                        decimal previousPrice = (year == 2025) ? originalPrices[i] : originalPrices[i] - originalPrices[i] * (annualLossPercentages[i] / 100);
+                        decimal loss = previousPrice * (annualLossPercentages[i] / 100);
+                        originalPrices[i] -= loss;
+                        totalLosses[i] += loss;
+                        Console.Write($"{totalLosses[i]:0.00}".PadRight(15));
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("\nCelková roční ztráta za každé auto od roku 2025 do roku 2030 (včetně benzínu a servisu):");
+                Console.WriteLine(new string('-', 50));
+
+                Console.Write("Rok".PadRight(15));
+                foreach (var carName in carNames)
+                {
+                    Console.Write($"{carName.PadRight(15)}");
+                }
+                Console.WriteLine();
+
+                decimal[] totalCosts = new decimal[carCount];
+                for (int year = 2025; year <= 2030; year++)
+                {
+                    Console.Write($"{year}".PadRight(15));
+                    for (int i = 0; i < carCount; i++)
+                    {
+                        totalCosts[i] += totalLosses[i] + annualServiceCosts[i] + annualFuelCosts[i];
+                        Console.Write($"{totalCosts[i]:0.00}".PadRight(15));
+                    }
+                    Console.WriteLine();
+                }
             }
-            Console.WriteLine();
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Chyba! Nebylo stisknuto tlačítko ENTER.");
+                Thread.Sleep(3000);
+                Console.Clear();
+            }
         }
     }
-}
-
-// Třída pro uchování informací o voze
-class Car
-{
-    public string Name { get; set; } // Název vozu
-    public int PurchaseYear { get; set; } // Rok koupě vozu
-    public decimal OriginalPrice { get; set; } // Původní cena vozu
-    public decimal AnnualLossPercentage { get; set; } // Procentuální roční ztráta ceny vozu
 }
